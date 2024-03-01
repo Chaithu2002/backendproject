e5a4e3ebc71699751f5b1555e20d5371b98618e4
Chaithu2002 <chaithuchowdhary333@gmail.com>
2024-03-01 11:25:45 +0530

    MODIFIED: password encryption for user

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,4 +1,5 @@
 import User from '../models/user.model';
+import bcrypt from 'bcrypt';
 
 // user registration validation
 export const register = async (body) => {
@@ -8,6 +9,8 @@ export const register = async (body) => {
     throw new Error("EmailId is already registered, use another EmailID");
   }else{
     const data = await User.create(body);
+    const encryptPassword = await bcrypt.hash(body.password,10);
+    data.password = encryptPassword;
     return data;
   }
 
