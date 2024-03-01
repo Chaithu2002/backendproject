43762cc075e086a4b7574e81654573bf8db7f2a2
Chaithu2002 <chaithuchowdhary333@gmail.com>
2024-03-01 12:54:50 +0530

    MODIFIED: registration validation using encryption

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,6 @@
 import { string } from '@hapi/joi';
 import { Schema, model } from 'mongoose';
-
+import bcrypt from 'bcrypt';
 const userSchema = new Schema(
   {
     fname:{
@@ -20,5 +20,17 @@ const userSchema = new Schema(
     timestamps: true
   }
 );
+
+// saving the encrypted password before saving in database
+userSchema.pre('save',async function(next){
+  try{
+    const salt = await bcrypt.genSalt(10);
+    const encryptPassword = await bcrypt.hash(this.password,salt);
+    this.password = encryptPassword;
+  }catch(error){
+    next(error);
+  }
+  
+});
 
 export default model('User', userSchema);
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -9,8 +9,6 @@ export const register = async (body) => {
     throw new Error("EmailId is already registered, use another EmailID");
   }else{
     const data = await User.create(body);
-    const encryptPassword = await bcrypt.hash(body.password,10);
-    data.password = encryptPassword;
     return data;
   }
 
